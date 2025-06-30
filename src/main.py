1587b5810cae5b8a437212611a11e11ce56eb5cb
opsullivan85 <opsullivan@wpi.edu>
2025-06-30 02:23:26 -0400

    basic text-extraction working, can query llm

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+import tempfile
 import os
 from typing import Optional
 from pathlib import Path
+from dotenv import load_dotenv
+from google import genai
+
+from text_extraction import get_listing_text
+
 
 def compile_latex(tex_path: str) -> Optional[str]:
     """turns the latex file (*.tex) at tex_path into a PDf
@@ -18,11 +24,26 @@ def compile_latex(tex_path: str) -> Optional[str]:
         return None
 
     return str(Path(tex_path).with_suffix(".pdf"))
-    
+
+
+
     
 
 def main():
-    compile_latex("c:/Users/Owen/git_projects/resume-customizer/base-resume/resume.tex")
+    url = "https://www.symbotic.com/careers/open-positions/R4550/"
+    load_dotenv()
+    
+    client = genai.Client()
+
+    header = "Explain an ideal candidate for the below job listing:\n\n"
+    body = get_listing_text(url)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=header+body
+    )
+    print(response.text)
+
+    # compile_latex("c:/Users/Owen/git_projects/resume-customizer/base-resume/resume.tex")
 
 if __name__ == "__main__":
     main()
